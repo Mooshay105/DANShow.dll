352824698e5561812d78a896778f595248956ee8
Malcolm <137590820+HydrogenOS1@users.noreply.github.com>
2024-01-09 16:50:21 +1000

    Upload The Main DanShow Files

---
+++ src/DanShow.cs
@@ -0,0 +1,25 @@
+﻿//Made by Malcolm, 2023. Site: malcolmjh.com
+using System;
+namespace DanShow;
+class DanShow
+{
+    static void Main()
+    {
+        Console.WriteLine("Starting DANShow...");
+        Console.WriteLine("Adding More Dans...");
+        Console.WriteLine("Done!");
+        Console.WriteLine("Starting Danplane Stream...");
+        Console.WriteLine("Done!");
+        Console.WriteLine("Starting Youtube Stream...");
+        Console.WriteLine("Done!");
+        Console.WriteLine("Starting Twitch Stream...");
+        Console.WriteLine("Done!");
+        Console.WriteLine("Starting kick Stream...");
+        Console.WriteLine("Done!");
+        Console.WriteLine("resuming selected...");
+        Console.WriteLine("Done!");
+        Console.WriteLine("Started The DANShow!");
+        Console.WriteLine("©2023 Malcolm, malcolmjh.com");
+    }
+}
+//Made by Malcolm, 2023. Site: malcolmjh.com
